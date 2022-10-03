9584c9e0be21f64265f1eeb1312db4339ae8589e
Felipe Martins <6557756+fefas@users.noreply.github.com>
2022-10-03 21:21:44 +0200

    Ensure reponse immutability after all tests

--- packages/tests/Http/Message/Response/TestCase.php
+++ packages/tests/Http/Message/Response/TestCase.php
@@ -3,16 +3,21 @@
 namespace Bauhaus\Tests\Http\Message\Response;
 
 use Bauhaus\Http\ResponseFactory;
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
 use Psr\Http\Message\ResponseFactoryInterface as PsrResponseFactory;
 use Psr\Http\Message\ResponseInterface as PsrResponse;
 
-abstract class ResponseTestCase extends TestCase
+abstract class TestCase extends PhpUnitTestCase
 {
-    use ResponseStatusCodesDataProvider;
-
     protected PsrResponseFactory $factory;
     protected PsrResponse $response;
+    private PsrResponse $responseCopy;
+
+    /** @before */
+    public function copyResponse(): void
+    {
+        $this->responseCopy = clone $this->response;
+    }
 
     /** @before */
     public function setUpResponse(): void
@@ -24,5 +29,11 @@ abstract class ResponseTestCase extends TestCase
     public function setUpFactory(): void
     {
         $this->factory = new ResponseFactory();
+    }
+
+    /** @after */
+    public function assertImmutability(): void
+    {
+        $this->assertEquals($this->responseCopy, $this->response, 'Immutability assertion');
     }
 }
